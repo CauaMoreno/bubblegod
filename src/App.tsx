9b75485c9e7485642ff750e959dd1fecd56ae1b7
Cauã Moreno <cauam7344@gmail.com>
2025-01-25 02:42:14 -0300

    adiciona função de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import { useState, useEffect } from 'react'
+import { createClient, Session } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-function App() {
-  const [count, setCount] = useState(0)
+const supabaseUrl = 'https://hcsmsnyvmcgkgvnppedi.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjc21zbnl2bWNna2d2bnBwZWRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3NzAxMjksImV4cCI6MjA1MzM0NjEyOX0.hjWIEc7zSW5xL7X2tHydujCl55yDPWY6aT30hi-80NM'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default function App() {
+  
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (
+    <div>Logged in!
+      <button onClick={addDataToUser}>SET</button>
+      <button onClick={getDataFromUser}>GET</button>
+    </div>
+
   )
+  }
+  async function addDataToUser(event: any) {
+    //event.preventDefault();
+    const { data, error } = await supabase.from('user').insert([
+      { uuid:session?.user.id, detergente: 15 }
+    ]);
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+    }
+  }
+
+  async function getDataFromUser(event: any) {
+    //event.preventDefault();
+
+    const { data, error } = await supabase.from('user').select().eq('uuid', session?.user.id).single()
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+    }
+  }
 }
 
-export default App
+
+
