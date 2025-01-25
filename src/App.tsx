c69505c9c30bfe7545a2101935477be5c14aa583
Miguel-Dias-112 <miguel.dias@estudante.ufjf.br>
2025-01-25 00:19:10 -0300

    configura interface primaria

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Guns from "./componentes/Gun/Guns"
 function App() {
-  const [count, setCount] = useState(0)
+  const [Detergentes, setCount] = useState(0)
 
   return (
     <div className='App'> 
+      <header>
+        <span>Bubble Gun</span>
+        <div className='detergentes'>
+          <img></img>
+          <span>{Detergentes}</span>
+        </div>
+      </header>
       <div className='ArmasContainer'>
             <Guns
                 url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8217AgF0fmdDqiONeR7PO0kJXF9m85VrBdA&s'
